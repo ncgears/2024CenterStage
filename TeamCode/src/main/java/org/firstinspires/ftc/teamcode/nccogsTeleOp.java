213dfabca906fbb58edd592c0363f081f0ca90ac
ncgears <1918ncgears@gmail.com>
2023-09-12 23:14:16 -0400

    Clean up field centric mecanum drive. Ditched hardware initializer (crashing)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nccogsTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nccogsTeleOp.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name="NCCOGS TeleOp", group="Jim")
+@Disabled
 public class nccogsTeleOp extends LinearOpMode {
     nccogsHardware robot = new nccogsHardware();
 
